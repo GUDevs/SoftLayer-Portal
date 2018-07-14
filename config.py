9d1bb98ba6939b2e8281af4a6833ffed5af645ad
GUDevs <groundup365@gmail.com>
2018-07-14 05:17:49 -0400

    Set Key

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DEBUG = False
 CSRF_ENABLED = True
-SECRET_KEY = 'my_secret_key' # Set this to something random
-OTP_SECRET = 'my_opt_key' # Set this to something else random
+SECRET_KEY = '54647135708' # Set this to something random
+OTP_SECRET = '90046434246' # Set this to something else random
 PAGE_SIZE = 25
 SQLALCHEMY_DATABASE_URI  = 'sqlite:////usr/local/slick/slick.db'
 #SQLALCHEMY_DATABASE_URI = 'postgresql://slick:slick@localhost/slick'
